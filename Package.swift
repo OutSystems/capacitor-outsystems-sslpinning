dd7f5bd4f2baf0def3debab0670545ae60e187d1
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-11-25 11:35:40 +0000

    fix(ios): SPM setup
    Can't seem to use our TrustKit repo as a Swift Package on this Objective-C plugin, but inserting the xcframework directly (already used for CocoaPods) seems to do the trick.

--- Package.swift
+++ Package.swift
@@ -10,15 +10,19 @@ let package = Package(
             targets: ["OutSystemsSSLPinningPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0-beta"),
-        .package(url: "https://github.com/OutSystems/TrustKit", exact: "3.0.7+1.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0-beta")
     ],
     targets: [
+        .binaryTarget(
+            name: "TrustKit",
+            path: "ios/TrustKit.xcframework" // or relative path
+        ),
         .target(
             name: "OutSystemsSSLPinningPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                "TrustKit"
             ],
             path: "ios/Sources/OutSystemsSSLPinningPlugin",
             publicHeadersPath: "include")
